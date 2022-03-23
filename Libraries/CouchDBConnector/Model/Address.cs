965ee8ccf637ef4483e064211a7a9e82227ba66a
Bartosz Klimowicz <klimowicz.bartek@gmail.com>
2022-03-23 19:17:13 +0100

    Removed some things

--- Libraries/CouchDBConnector/Model/Address.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CouchDBConnector.Model
-{
-    public class Address
-    {
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string Zipcode { get; set; }
-    }
-}
