deafd026f72355f7aedc1ceb424552f68624babd
Bartosz Klimowicz <klimowicz.bartek@gmail.com>
2022-07-30 00:46:17 +0200

    React redux template

--- AngularPage/src/app/account/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./login/login.component";
-export * from "./register/register.component";
-export * from "./account.module";
-export * from "./account-routing.module";
-export * from "./layout.component";
